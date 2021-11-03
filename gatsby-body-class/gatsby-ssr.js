842c5e91a7daf44161c2f1d00a705aaf82d54950
Victor Garcia Mestre <vgmestre@gmail.com>
2021-11-03 15:33:07 +0100

    finished working color mode script in ssr

--- gatsby-body-class/gatsby-ssr.js
+++ gatsby-body-class/gatsby-ssr.js
@@ -1,7 +1,38 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/ssr-apis/
- */
+import React from "react"
+import { COLOR_MODE_KEY } from "./src/constants"
 
-// You can delete this file if you're not using it
+function setColorsByTheme() {
+  const colorModeKey = "🔑"
+
+  const mql = window.matchMedia("(prefers-color-scheme: dark)")
+  const prefersDarkFromMQ = mql.matches
+  const persistedPreference = localStorage.getItem(colorModeKey)
+
+  let colorMode = "light"
+
+  const hasUsedToggle = typeof persistedPreference === "string"
+
+  if (hasUsedToggle) {
+    colorMode = persistedPreference
+  } else {
+    colorMode = prefersDarkFromMQ ? "dark" : "light"
+  }
+
+  document.body.className = colorMode
+}
+
+const ScriptTag = () => {
+  const boundFn = String(setColorsByTheme).replace("🔑", COLOR_MODE_KEY)
+
+  let calledFunction = `(${boundFn})()`
+
+  //calledFunction = Terser.minify(calledFunction).code
+
+  // eslint-disable-next-line react/no-danger
+  return <script dangerouslySetInnerHTML={{ __html: calledFunction }} />
+}
+
+export const onRenderBody = ({ setPreBodyComponents, setHeadComponents }) => {
+  //setHeadComponents(<FallbackStyles />)
+  setPreBodyComponents(<ScriptTag key="script-tag" />)
+}
