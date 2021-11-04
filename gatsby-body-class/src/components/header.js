c3a8c688078edc4aa9450e2abe4b36ef718a5d7d
Victor Garcia Mestre <vgmestre@gmail.com>
2021-11-04 10:36:01 +0100

    removed default colors

--- gatsby-body-class/src/components/header.js
+++ gatsby-body-class/src/components/header.js
@@ -1,42 +1,36 @@
-import * as React from "react"
-import PropTypes from "prop-types"
-import { Link } from "gatsby"
+import * as React from "react";
+import PropTypes from "prop-types";
+import { Link } from "gatsby";
+import DarkToggle from "./dark-toggle";
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
+  <header>
     <div
       style={{
         margin: `0 auto`,
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
       }}
     >
       <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+        <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
           {siteTitle}
         </Link>
       </h1>
+      <DarkToggle />
     </div>
   </header>
-)
+);
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
-}
+};
 
 Header.defaultProps = {
   siteTitle: ``,
-}
+};
 
-export default Header
+export default Header;
